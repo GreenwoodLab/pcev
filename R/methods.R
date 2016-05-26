3f2c5888991e5e7c13df21ade7619aaeebaa55a7
MTurgeon <maxime.turgeon@mail.mcgill.ca>
2016-05-26 16:36:00 -0400

    implement Chiani's algorithm

--- R/methods.R
+++ R/methods.R
@@ -556,3 +556,44 @@ JohnstoneParam <- function(p, m, n) {
   
   return(c(mu, sigma))
 }
+
+# Chiani 2016 iterative algorithm----
+# Incomplete Beta function
+incompleteBeta <- function(x, alpha, beta) {
+  pbeta(x, shape1 = alpha, shape2 = beta) * exp(lgamma(alpha) + lgamma(beta) - lgamma(alpha + beta))
+}
+
+# CDF computation
+pRoy <- function(x, s, m, n) {
+  # Initialize matrix
+  A <- matrix(0, ncol=s, nrow=s)
+  b <- rep_len(0, s)
+  
+  if (s != 1) {
+    for (i in 1:(s-1)) {
+      b[i] <- 0.5*incompleteBeta(x, m + i, n + 1)^2
+      for (j in i:(s-1)) {
+        b[j+1] <- ((m+j)*b[j] - incompleteBeta(x, 2*m+i+j, 2*n+2))/(m+j+n+1)
+        A[i,j+1] <- incompleteBeta(x, m+i, n+1) * incompleteBeta(x, m+j+1, n+1) - 
+          2*b[j+1]
+      }
+    }
+  }
+  
+  if (s %% 2) {
+    A <- rbind(A, 0)
+    extraCol <- rep_len(0, s+1)
+    for (i in 1:s) {
+      extraCol[i] <- incompleteBeta(x, m+i, n+1)
+    }
+    A <- cbind(A, extraCol)
+  }
+  A <- A - t(A)
+  
+  # Compute scaling constant
+  C <- pi^(0.5*s) * exp(sum(lgamma(0.5*(1:s+2*m+2*n+s+2)) - lgamma(0.5*(1:s)) - 
+                              lgamma(0.5*(1:s+2*m+1)) - lgamma(0.5*(1:s+2*n+1))))
+  
+  result <- C * sqrt(abs(det(A)))
+  return(result)
+}
