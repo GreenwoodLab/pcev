419e0eeec54bd99fc9d6afe09d2510c41ce9ff15
Max Turgeon <turgeon.maxime@gmail.com>
2018-11-19 14:55:14 -0600

    refactor variance estimation

--- R/estimatePcev.R
+++ R/estimatePcev.R
@@ -30,11 +30,11 @@ estimatePcev.PcevClassical <- function(pcevObj, shrink, index, ...) {
   p <- ncol(Y)
 
   # Variance decomposition
-  fit <- lm.fit(cbind(pcevObj$X, pcevObj$Z), Y)
-  Yfit <- fit$fitted.values
+  # fit <- lm.fit(cbind(pcevObj$X, pcevObj$Z), Y)
+  Yfit <- getFitted(cbind(pcevObj$X, pcevObj$Z), Y)
   res <- Y - Yfit
-  fit_confounder <- lm.fit(cbind(rep_len(1, N), pcevObj$Z), Y)
-  Yfit_confounder <- fit_confounder$fitted.values
+  # fit_confounder <- lm.fit(cbind(rep_len(1, N), pcevObj$Z), Y)
+  Yfit_confounder <- getFitted(cbind(rep_len(1, N), pcevObj$Z), Y)
   
   Vr <- crossprod(res)
   Vm <- crossprod(Yfit - Yfit_confounder, Y)
@@ -143,12 +143,12 @@ estimatePcev.PcevSingular <- function(pcevObj, shrink, index, ...) {
   
   
   # Variance decomposition
-  fit <- lm.fit(cbind(pcevObj$X, pcevObj$Z), Y)
-  Yfit <- fit$fitted.values
-  Intercepts <- fit$coefficients[1,]
+  # fit <- lm.fit(cbind(pcevObj$X, pcevObj$Z), Y)
+  Yfit <- getFitted(cbind(pcevObj$X, pcevObj$Z), Y)
+  # Intercepts <- fit$coefficients[1,]
   res <- Y - Yfit
-  fit_confounder <- lm.fit(cbind(rep_len(1, N), pcevObj$Z), Y)
-  Yfit_confounder <- fit_confounder$fitted.values
+  # fit_confounder <- lm.fit(cbind(rep_len(1, N), pcevObj$Z), Y)
+  Yfit_confounder <- getFitted(cbind(rep_len(1, N), pcevObj$Z), Y)
   
   Vr <- crossprod(res)
   Vm <- crossprod(Yfit - Yfit_confounder, Y)
@@ -163,11 +163,6 @@ estimatePcev.PcevSingular <- function(pcevObj, shrink, index, ...) {
   Xpp <- svdC$u
   singWeights <- Xp %*% Xpp
   largestRoot <- max(crossprod(singWeights,  Vm %*% singWeights))
-  #singPCEV <- (Y) %*% singWeights[, 1] 
-  
-  #singVIMP <- unlist(lapply(1:ncol(Y), function(x) cor(singPCEV, (Y)[,x]))) 
-  
-  
   
   out <- list("residual" = Vr,
               "model" = Vm,
